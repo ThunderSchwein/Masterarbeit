2fa289a89fc69c757ae8a3a6cc81cb20bc537abf
Konstantin <orangenpopo@web.de>
2016-11-11 20:37:31 +0100

    Create Units.py

---
+++ Units.py
@@ -0,0 +1,39 @@
+# Physical Constants
+
+# Vacuum permittivity 8.854e-12 As/Vm
+e0 = .1
+#e0 = 55.263e-3 # (e-)/(V*nm) 
+
+# Titanum Dioxide Properties
+
+# Metal Properties 
+# [Pt]
+Pt_phi = 5.2 #V [ @ 5.0 - 5.5 eV ]
+
+# Pt/TiO2 barrierheight
+barrier = 1.9 #eV
+
+# Semiconductor Properties
+# [TiO2]
+# Electron affinity
+Ti_chi = 4.0 	#V
+# Dieelectricity constant factor
+Ti_es = 100	#[ @ 86 - 173 ]
+# Doping density [1/nm**3]
+Ti_nd = 0.5		#[5e26 /m**3]
+
+
+# Strungaru Paper
+
+# Filament Length [nm]
+Length = 10	# 5µm
+
+# Oxygen Vacancy Drift Mobility [nm**2 / Vs]
+Mu = 1000		# 1e-15 m**2 / VS
+# Electroforming break-even Voltage [V/nm]
+E_min = 4e-4 	# 4e5 V/m
+
+# Conductivity [Simens = 1/R = Ampere/nm]
+Sigma1 = 3.5e-3
+Sigma2 = 6e-3
+Sigma3 = 1e-3
