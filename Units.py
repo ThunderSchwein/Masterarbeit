f5b9dd066c144be06d44b262c81752ab32e9c15c
Konstantin <orangenpopo@web.de>
2016-11-23 22:08:34 +0100

    Update Units.py

--- Units.py
+++ Units.py
@@ -1,35 +1,45 @@
+#Length Unit [m]
+ds = 1e-9	# 1 nm
+T  = 300	# K
+
 # Physical Constants
+Kb = 8.617e-6
+
+# Electron Charge [Coulomb]
+e =-1.6021766208e-19
 
 # Vacuum permittivity 8.854e-12 As/Vm
-e0 = .1
-#e0 = 55.263e-3 # (e-)/(V*nm) 
+e0 = 55.263e-3 # (e-)/(V*nm) 
 
 # Titanum Dioxide Properties
 
 # Metal Properties 
 # [Pt]
-Pt_phi = 5.2 #V [ @ 5.0 - 5.5 eV ]
+Platinum_WorkFunktion = 5.2 #V [ @ 5.0 - 5.5 eV ]
 
 # Pt/TiO2 barrierheight
-barrier = 1.9 #eV
+BarrierHeight = 1.9 #eV
 
 # Semiconductor Properties
 # [TiO2]
 # Electron affinity
-Ti_chi = 4.0 	#V
-# Dieelectricity constant factor
-Ti_es = 100	#[ @ 86 - 173 ]
+Titanim_ElectronAffinity = 4.0 	#V
+# Dielectricity constant factor
+Titanium_DielectricityFactor = 70	#[ @ 86 - 173 ]
 # Doping density [1/nm**3]
-Ti_nd = 0.5		#[5e26 /m**3]
+Titanium_DopingDensity = 1e-4	#[1e23 /m**3]
+Titanium_DopingCharge = 2
+# Oxygen Vacancy Mobility
+Titanium_DopingMobility = 2.5 	#[nm**2 / V/s]
 
-
+#------------------------------------------
 # Strungaru Paper
 
 # Filament Length [nm]
-Length = 10	# 5µm
+DeviceLength = 5000	# 5µm
 
 # Oxygen Vacancy Drift Mobility [nm**2 / Vs]
-Mu = 1000		# 1e-15 m**2 / VS
+Mobility = 1000		# 1e-15 m**2 / VS
 # Electroforming break-even Voltage [V/nm]
 E_min = 4e-4 	# 4e5 V/m
 
