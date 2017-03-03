224ec77fdf5edb663ec4caad78a9572a7a7f3879
Konstantin <orangenpopo@web.de>
2017-03-03 01:12:25 +0100

    Update Schottky.py

--- Schottky.py
+++ Schottky.py
@@ -7,7 +7,6 @@ Please take note that the "Initial Conditions"-Graph is only valid without
 applied external voltage.
 """
 
-
 from numpy import *
 import matplotlib.pyplot as plt
 from copy import deepcopy
@@ -38,9 +37,8 @@ class Schottky(object) :
 		# Initail Charge density & Depletion Region Width
 		self.ChargeDensity = zeros(nx)
 		if(self.Bias > -u.BarrierHeight):
-			DepletionRegionCharge = u.Titanium_InversionCharge
-			self.W = (2*(u.Titanium_DielectricityFactor*u.e0)*abs(u.BarrierHeight+self.Bias)/(abs(u.Titanium_InversionCharge)*u.Titanium_InversionDensity))**.5
-			self.ChargeDensity[:int(self.W/u.DeviceLength*nx)] = u.Titanium_InversionCharge*u.Titanium_InversionDensity
+			self.W = 5
+			self.ChargeDensity = 0
 		else: 
 			DepletionRegionCharge = u.Titanium_DopingCharge
 			self.W = (2*(u.Titanium_DielectricityFactor*u.e0)*abs(u.BarrierHeight+self.Bias)/(u.Titanium_DopingCharge*u.Titanium_DopingDensity))**.5
@@ -49,65 +47,48 @@ class Schottky(object) :
 		#Initial Potential Calculation
 		self.Phi = zeros(nx)
 		self.Phi[1] = self.Phi[0] = u.BarrierHeight+self.Bias; self.Phi[-1] = 0
-		self.Phi = solverLU_backward(self.X, self.Phi, self.ChargeDensity, self.Dieelek)
+		solverLU_backward(self)
 
 		#Electrical Field
-		E = E_Field(self.X, self.Phi)
+		self.E = zeros(nx)
+		self.E = E_Field(self.X, self.Phi, self.E)
 		
 		# Inital conditions Plot
 		plt.figure("Initial Conditions")
 		plt.plot(self.X, self.ChargeDensity)
 		plt.plot(self.X, self.Phi,color = "G")
-		plt.plot(self.X, E, color = "R")
-		
-		#Mirror Charge Correction
-		#Phi = Phi - 1/((X+1e-1)*16*pi*u.e0*u.Titanium_DielectricityFactor)
-		
-		#Phi5 = solverLU_backward(X, Phi, ChargeDensity, Dieelek)
-		#plt.plot(X, Phi5, color = "Red")	
-		
+		plt.plot(self.X, self.E, color = "R")
+		return
 	#------------------------------------
 	def DopingDrift(self):
 		# 2. Calculate Doping Drift
-		DP = DriftRK( self.X , self.Phi, self.DopingDensity, self.dt)
-		"""
-		for i in range(Iterations-1) :
-			print(self.Phi[-1])
-			[self.ChargeDensity, self.W]  = ReloadDeltaPhi(self.X, DP, self.ChargeDensity, self.Phi, self.Dieelek, self.W, FermiLevel = self.Bias)
-			self.Phi[-1] = 0.0; self.Phi[-2] = 0.0
-			#print(i)
-			self.Phi = solverLU(self.X, self.Phi, self.ChargeDensity, self.Dieelek)
-			DP = DriftRK( self.X , self.Phi, DP, self.dt)
-		"""
-		[self.ChargeDensity, self.W]  = ReloadDeltaPhi(self.X, DP, self.ChargeDensity, self.Phi, self.Dieelek, self.W, FermiLevel = self.Bias)
+		DriftRK(self)
+		ReloadDeltaPhi(self)
 		self.Phi[-1] = 0.0; self.Phi[-2] = 0.0
-		self.Phi = solverLU_backward(self.X, self.Phi, self.ChargeDensity, self.Dieelek)
-		self.DopingDensity = DP
+		solverLU_backward(self)
+		return
 		
 	def PlotResults(self):
 		plt.figure("Oxygen Vacancy Movement")
 		
 		plt.plot(self.X, self.DopingDensity, color = "Grey")
 		plt.plot(self.X, self.ChargeDensity, color = "Red")
-		#plt.plot(self.X, DP, color = "Blue")
-
-		#plt.plot(X, E, color = "Purple")
-		#plt.plot(X, DP-ChargeDensity)
 
 		plt.figure("Potentials")
-		E2 = E_Field(self.X, self.Phi)
-		plt.plot(self.X, E2, color = "Blue")
-		plt.plot(self.X, self.DopingDensity, color = "Grey")
-
-		self.Phi[-1] = 0.0; self.Phi[-2] = 0.0
-		Phi5 = solverLU_backward(self.X, self.Phi, self.ChargeDensity, self.Dieelek)
-		plt.plot(self.X, Phi5, color = "Red")
 		
 		self.Phi[1] = self.Phi[0] = (u.BarrierHeight+self.Bias); self.Phi[-1] = 0.0
 		self.Phi = solverLU(self.X, self.Phi, self.ChargeDensity, self.Dieelek)
 		plt.plot(self.X, self.Phi, color = "Green")
+		
+		plt.plot(self.X, self.E, color = "Blue")
+		plt.plot(self.X, self.DopingDensity, color = "Grey")
 
+		solverLU_backward(self)
+		plt.plot(self.X, self.Phi, color = "Red")
+		
 		plt.show()
+		
+		return
 		
 	def WriteToFile(self, File):
 		savetxt(File, (self.Phi, self.DopingDensity, self.ChargeDensity), newline="\n")
