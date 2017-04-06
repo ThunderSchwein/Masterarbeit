cb4eda0cd1f393beec093c321e6ea1e4c000966f
Konstantin <orangenpopo@web.de>
2017-04-06 22:08:16 +0200

    Update Plotter.py

--- Plotter.py
+++ Plotter.py
@@ -9,39 +9,25 @@ import Schottky as S
 from Run import Init_Sample, Import
 
 Sample = Init_Sample(0, 1, 50001)
-#Import(Sample, "V=10-10(1)T=2000s")
-Import(Sample, "V=0T=200min")
+Import(Sample, "V=10(5)T=66000s")
+#Import(Sample, "V=0T=200min")
 Sample.PlotResults()
-"""
-D =  loadtxt("Resistance0")
-#[(T, R0, R_1, R1, C, Sample.W)]
-#print(D)
 
+D =  loadtxt("Resistance10V(0)")
 
-T	= D[:,0]
-R0	= D[:,1]
-R_1	= D[:,2]
-R1  = D[:,3]
-C   = D[:,4]
-W   = D[:,5]
-V   = D[:,6]
-
+T	= D[0,:]
+R   = D[1,:]
+C   = D[2,:]
 
 plt.figure("Resistance")
-
-R2, = plt.plot(T, R0/u.Sigma1, label = "Original R")
-R3, = plt.plot(T, R_1/u.Sigma3, label = "High R")
-R4, = plt.plot(T, R1/u.Sigma2, label = "Low R")
-
-Rtotal = R1/u.Sigma2 + R_1/u.Sigma3 + R0/u.Sigma1
-R5, = plt.plot(T, Rtotal, label = "Total R")
-R6, = plt.plot(T, V/Rtotal*1000, label = "Curent/mA")
+R5, = plt.plot(T/10, R*1e-6, label = "Total R [MOhm]")
+R6, = plt.plot(T/10, C, label = "Capacity")
+plt.xlabel("t[min]")
 plt.legend()
 
-plt.figure("Capacity")
-plt.plot(T, C)
-plt.plot(T, W)
-plt.figure("Mott-Schottky")
-plt.plot(V, 1/(C*C))
-"""
+#plt.figure("Capacity")
+#plt.plot(T, C)
+#plt.figure("Mott-Schottky")
+#plt.plot(V, 1/(C*C))
+
 plt.show()
