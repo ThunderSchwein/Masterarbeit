c22febd16397d541dd4ce4ca6b66ccd25e224478
Konstantin <orangenpopo@web.de>
2017-04-18 07:20:01 +0200

    Update Plotter.py

--- Plotter.py
+++ Plotter.py
@@ -3,27 +3,34 @@ import matplotlib.pyplot as plt
 from copy import deepcopy
 import Units as u
 from SolverLU import *
-from DopingDrift import DriftRK, Diffusion
+#from DopingDrift import DriftRK, Diffusion
 from ChargeRedistribution import *
 import Schottky as S
 from Run import Init_Sample, Import
 
-Sample = Init_Sample(0, 1, 50001)
-Import(Sample, "V=10(5)T=66000s")
+Sample = Init_Sample(0, 1, 5001)
+Import(Sample, "V=10(01)T=100s")
 #Import(Sample, "V=0T=200min")
 Sample.PlotResults()
-
-D =  loadtxt("Resistance10V(0)")
+plt.show()
+D =  loadtxt("Resistance10V(01)")
 
 T	= D[0,:]
-R   = D[1,:]
-C   = D[2,:]
+V   = D[1,:]
+R   = D[2,:]
 
 plt.figure("Resistance")
-R5, = plt.plot(T/10, R*1e-6, label = "Total R [MOhm]")
-R6, = plt.plot(T/10, C, label = "Capacity")
+R5, = plt.plot(V, R*1e-6, label = "Total R [MOhm]")
 plt.xlabel("t[min]")
 plt.legend()
+
+plt.figure("Current")
+R7, = plt.plot(V[int(len(V)/2):], V[int(len(V)/2):]/(R[int(len(V)/2):]*1e-6), label = "Current_rising")
+R6, = plt.plot(V[:int(len(V)/2)], V[:int(len(V)/2)]/(R[:int(len(V)/2)]*1e-6), label = "Current_decreasing")
+plt.xlabel("U[V]")
+plt.legend()
+
+#savetxt("Resistance10V(001)", (T, V, R), newline="\n")
 
 #plt.figure("Capacity")
 #plt.plot(T, C)
